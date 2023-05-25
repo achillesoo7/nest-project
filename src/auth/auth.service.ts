35f5081890c403c965ddd48a46b05e5a57d4443e
achillesoo7 <sjsumit007@gmail.com>
2023-05-25 22:11:11 +0530

    initial project setup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  signup() {
+    return { message: 'I am signup' };
+  }
+
+  signin() {
+    return { message: 'I am signin' };
+  }
+}
